fb4b6b7cae383c41294a27afee02547d34716623
mizushin53 <taurus.nebula@gmail.com>
2022-07-21 12:55:29 +0900

    ファイル整理

--- index.ts
+++
@@ -1,8 +0,0 @@
-const discord = require('discord.js');
-const client = new discord.Client();
-const discordbtn = require('discord.js-buttons')(client);
-
-const button = new discordbtn.MessageButton()
-    .setStyle('green')
-    .setLabel('ファイル更新・読込')
-    .setID('scan');
